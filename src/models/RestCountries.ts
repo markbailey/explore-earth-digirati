13b772db6720e715575606532f8183f7c99962ff
Mark Bailey <markbailey78@yahoo.co.uk>
2023-04-14 20:33:11 +0100

    Add abort signal to api calls

--- src/models/RestCountries.ts
+++ src/models/RestCountries.ts
@@ -4,8 +4,8 @@ const query = `?fields=name,region,subregion,population,capital,currencies,flags
 const sortCountries = (countries: RestCountry[]) =>
   countries.sort((a, b) => a.name.common.localeCompare(b.name.common));
 
-const fetchCountries = (slug: string) =>
-  fetch(`${API_URL}${slug}`)
+const fetchCountries = (slug: string, signal?: AbortSignal) =>
+  fetch(`${API_URL}${slug}`, { signal })
     .then((response) => {
       // Throw an error if the response status is not 200 here as otherwise error
       // being passed to the next .then() call
@@ -15,8 +15,12 @@ const fetchCountries = (slug: string) =>
     .then(sortCountries);
 
 class RestCountries {
+  static readonly controller: AbortController = new AbortController();
+  static readonly signal: AbortSignal = this.controller.signal;
+
+  static abort = () => this.controller.abort();
   static async region(region: string) {
-    const countries = await fetchCountries(`region/${region}${query}`);
+    const countries = await fetchCountries(`region/${region}${query}`, this.signal);
     return {
       countries,
       country: (country: string) =>
